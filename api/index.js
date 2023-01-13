1d174e51205f2beee62f2891976dedbabbea321a
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-12 21:02:22 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import { Users } from './users';
 
 const express = express();
 app.use(cors());
 
-app.get("/api/", ( req, res ) => {
-	res.json("Hellloo World Darknessssssss!!!");
+app.get("/api/users", ( req, res ) => {
+	// res.json("Hellloo World Darknessssssss!!!");
+
+	const { q } = req.query;
+	const keys = ["first_name", "last_name", "email"];
+
+	const search = ( data ) => {
+		return data.filter(( item ) => 
+			keys.some(( key ) => item[key].includes(q))
+		);
+	};
+
+	res.json(search(Users).splice(0, 10));
 });
 
 app.listen(5000, () => console.log("API is working in port 5000!!!!!!!!!!!"));
